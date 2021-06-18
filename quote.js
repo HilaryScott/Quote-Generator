1129d0a39fa1328cabb6e845c7c241fcc565a0ca
hilary.scott <hscott721@gmail.com>
2021-06-18 14:41:00 -0400

    Create quote.js
    Created main quotes object

---
+++ quote.js
@@ -0,0 +1,43 @@
+let quote,
+    source,
+    citation,
+    year;
+
+let quotes = [
+    {
+      quote: 'Somewhere, something incredible is waiting to be known.',
+      source: 'Carl Sagan',
+      citation: ,
+      year:
+    },
+    {
+      quote: 'Science is not only compatible with spirituality; it is a profound source of spirituality.',
+      source: 'Carl Sagan',
+      citation: 'The Demon-Haunted World: Science as a Candle in the Dark',
+      year:
+    },
+    {
+      quote: 'The saddest aspect of life right now is that science gathers knowledge faster than society gathers wisdom.',
+      source: 'Issav Asimov',
+      citation: ,
+      year:
+    },
+    {
+      quote: 'That\'\s hot.',
+      source: 'Paris Hilton',
+      citation: 2000,
+      year:
+    },
+    {
+      quote: ' For I dipped into the Future, far as human eye could see; saw the vision of the world, and all the wonder that would be.',
+      source: 'Alfred, Lord Tennyson,',
+      citation: ,
+      year: 1842
+    },
+    {
+      quote: 'Where there is no vision, the people perish.',
+      source: 'Proverbs 29:18',
+      citation: ,
+      year:
+    },
+];
